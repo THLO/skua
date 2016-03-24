05aa3878b2b51685b9e86fd3ffb8ff04c92d578e
Thomas Locher <thamasta@gmx.ch>
2016-03-24 14:08:38 +0100

    Update skua.py

--- skua.py
+++ skua.py
@@ -5,17 +5,18 @@ Created on Mon March 2 11:05:00 2016
 """
 
 import urllib2, requests, time, random,sys
+import argparse
 from collections import OrderedDict
 
 # Version number
-version = '0.0.2'
+version = '0.0.3'
 
 # Default values for certain parameters:
 
 # The prefix is prepended to the original filename, resulting in the filename
 # that is used when storing the downloaded file.
 # By default, the prefix is the emtpy string.
-defaultPrefix = ''
+defaultDestination = ''
 
 # Only files are downloaded with a certain extension.
 # By default, the allowed extension is 'jpg'.
@@ -27,6 +28,35 @@ defaultExtension = 'jpg'
 defaultMinimumTimeout = 0.5
 defaultMaximumTimeout = 5.0
 
+# Site-specific parameters can be added in the form of key-value pairs.
+# By default, there are no such parameters.
+defaultParameter = ''
+
+""" Helper function to test if a string is an integer. """
+def isInt(string):
+    try: 
+        int(string)
+        return True
+    except ValueError:
+        return False
+
+"""
+An example for a filter:
+File containing [int]x[int] are dropped because they (often) refer to
+rescaled images.
+"""
+def filterRescaled(items):
+	filteredList = []
+	for item in items:
+		pos = item.rfind('x')
+		if not(pos >= 0 and pos < len(item)-1 and isInt(item[pos-1]) and isInt(item[pos+1])):
+			filteredList.append(item)
+	return filteredList
+
+"""
+A simple usage message is printed by this method.
+This will be replaced by argparse.
+"""
 def printUsage():
 	print "Usage: skua [URL] {prefix} {extension}"
 	print "* URL (mandatory): Website from which files should be downloaded."
@@ -40,12 +70,11 @@ if len(sys.argv) < 2:
 	printUsage()
 else:
 	url = sys.argv[1]
-	prefix = defaultPrefix if len(sys.argv) < 3 else sys.argv[2]
+	prefix = defaultDestination if len(sys.argv) < 3 else sys.argv[2]
 	extension = defaultExtension if len(sys.argv) < 4 else sys.argv[3]
 	minimumTimeout = defaultMinimumTimeout
 	maximumTimeout = defaultMaximumTimeout
 
-	#hdr = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}
 	hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'}
 	req = urllib2.Request(url, headers=hdr)
 	response = urllib2.urlopen(req)
@@ -53,7 +82,8 @@ else:
 	pieces = html.split("\"")
 	items = [piece for piece in pieces if piece.endswith(extension)]
 	itemsNoDuplicates = list(OrderedDict.fromkeys(items))
-	for item in itemsNoDuplicates:
+	filteredList = filterRescaled(itemsNoDuplicates)
+	for item in filteredList:
 		if item.startswith("//"):
 			item = "https:"+item
 		print "Downloading "+item+"..."
@@ -66,4 +96,3 @@ else:
 			time.sleep(random.uniform(minimumTimeout,maximumTimeout))
 		except:
 			pass
-		
